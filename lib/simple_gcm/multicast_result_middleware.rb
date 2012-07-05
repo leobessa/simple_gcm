4147b2f49c5621249a75d76e9223257812144644
Leonardo Bessa <leobessa@gmail.com>
2012-07-05 18:06:01 -0300

    try to fix MulticastResultMiddleware

--- lib/simple_gcm/multicast_result_middleware.rb
+++ lib/simple_gcm/multicast_result_middleware.rb
@@ -1,8 +1,9 @@
 module SimpleGCM
   class MulticastResultMiddleware < ::Faraday::Response::Middleware
     def on_complete(env)
+      puts env[:status]
       case env[:status]
-      when 200
+      when 200...300
         env[:gcm_multicast_result] = MulticastResult.new(JSON.parse(env[:body],:symbolize_names => true))
       else
         raise Error::ServerUnavailable, response_values(env)
